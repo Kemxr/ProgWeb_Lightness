4689de664f5afe60f221dbb7dd646485a3644a7b
Kemxr <marcbouriot@hotmail.com>
2025-01-28 15:51:26 +0100

    Lightness Done !

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import { generatePalette } from "./modules/util";
+import { transformHSLToShadow } from "./modules/util";
 import { Color } from "./modules/Color";
+import * as convert from "color-convert";
+import { Notyf } from "notyf";
+import "notyf/notyf.min.css";
 
 //En gros : L'utilisateur entre un code hexadécimal, la fonction generatePalette transforme ce hex en hsl pour pouvoir faire
 //les opérations nécéssaires (avoir un dégradé donc changer la luminosité), ensuite displayColors() créer des instances de Color 
@@ -7,19 +11,35 @@ import { Color } from "./modules/Color";
 //en hex pour pouvoir générer les éléments avec display().
 //Dans displayColors on ajoute la class "minimized" au header pour afficher la palette quand on submit le code hex
 
+//Créer une instance de la classe Notyf
+const notyf = new Notyf();
+
 const form = document.querySelector("form");
 const container = document.querySelector("main");
 const header = document.querySelector("header");
 
-//Partie à faire : Reset du container etc...
 
-const displayColors = (palette) => {
+const displayColors = (palette, inputValue) => {
     container.replaceChildren();
     header.classList.add("minimized");
     //Pour chaque palette on génère un élément
     palette.forEach(c => {
         new Color(c).display(container);
     });
+
+    //Marche mais pas très beau
+    //Changer le dégradé de l'arrière plan en fonction de la couleur
+    const start = convert.hsl.hex(palette[2]);
+    const mid = convert.hsl.hex(palette[10]);
+    const end = convert.hsl.hex(palette[18]);
+    document.body.style.background = `linear-gradient(-45deg, #${start}, #${mid}, #${end})`;
+    document.body.style.backgroundSize = `400% 400%`;
+
+    //Changer la couleur de l'ombre
+    const hsl = convert.hex.hsl(inputValue);
+    const transformedHSL = transformHSLToShadow(hsl);
+    const root = document.documentElement;
+    root.style.setProperty("--shadow-color", transformedHSL);
 }
 
 form.addEventListener("submit", e => {
@@ -30,9 +50,21 @@ form.addEventListener("submit", e => {
     //vérifie si l'utilisateur entre un code hex valide
     if (/^#[0-9A-F]{6}$/i.test(formInput.value)) {
         const palette = generatePalette(formInput.value);
-        displayColors(palette);
+        displayColors(palette,formInput.value);
     }else{
         console.error(`${formInput.value} is not a valid Hexadecimal color`)
+        notyf.error(`${formInput.value} is not a valid Hexadecimal color`);
     }
 
+    
+
+})
+
+//Copier le code hex
+container.addEventListener("click", async e => {
+    if (e.target.className === "color") {
+        const color = e.target.dataset.color;
+        await navigator.clipboard.writeText(color);
+        notyf.success(`copied ${color} to clipboard`);
+    }
 })
--- src/modules/util.js
+++ src/modules/util.js
@@ -16,14 +16,26 @@ export const generatePalette = (hex) => {
 
     //Boucle allant de 0 a 100 de 10 en 10 pour que la lightness soit multiple de 10
     //et a chaque boucle on push les même deux premier éléments en ajoutant la lightness qui monte
-    for (let i = 0; i <= 100; i+=5) {
+    for (let i = 0; i < 100; i+=5) {
         //on met des [] pour créer un tableau pour chaque itération de i
         colors.push([h,s,i]);
     }
     
-    // console.log(colors)
     //On retourne le tableau de colors pour pouvoir utiliser les fonctions de tableau dans app.js
     return colors;
     
+}
+
+export const transformHSLToShadow = (hsl) => {
+    const shadows = [];
+
+    //Rajoute ce qu'on a besoin derrière chaque valeur HSL
+    const first = `${hsl[0]}deg`;
+    const second = `${hsl[1]}%`;
+    const third = `${hsl[2]}%`;
+
+    shadows.push(first,second,third);
+    
+    return shadows;
 }
 
